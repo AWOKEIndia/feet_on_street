61dd7aa9c4ec4a7827b076c6e9de8bf2755e7481
Apavayan Sinha <info@apavayan.com>
2025-04-04 09:33:11 +0000

    feat: Add new doctype structures for Block, CFL Center, District, Project Phase, Region, and State

---
+++ feet_on_street/feet_on_street/doctype/cfl_center/test_cfl_center.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, AWOKE India and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCFLCenter(FrappeTestCase):
+	pass
