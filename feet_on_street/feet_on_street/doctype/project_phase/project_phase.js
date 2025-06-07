dd5de23d6df1efb3b308d7414c981f95a3f7a3e5
Apavayan Sinha <info@apavayan.com>
2025-06-07 14:51:16 +0530

    fix: update doctype fo Project Phase
    Add abbr for short code the name

--- feet_on_street/feet_on_street/doctype/project_phase/project_phase.js
+++ feet_on_street/feet_on_street/doctype/project_phase/project_phase.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2025, AWOKE India and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Project Phase", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Project Phase", {
+    project_phase: function(frm) {
+        if (frm.doc.__islocal) {
+			// add missing " " arg in split method
+			let parts = frm.doc.project_phase.split(" ");
+			let abbr = $.map(parts, function (p) {
+				return p ? p.substr(0, 1) : null;
+			}).join("");
+			frm.set_value("abbr", abbr);
+		}
+    },
+    refresh(frm) {
+        if (!frm.is_new()) {
+            frm.doc.abbr && frm.set_df_property("abbr", "read_only", 1);
+        }
+	},
+});
